140c6bb0351f4ca38c3312d3d35fde61fa24c6a2
Josef Lambley <17LamblJ@fulford.york.sch.uk>
2022-09-26 15:18:31 +0100

    built the code

--- recursion/Program.cs
+++ recursion/Program.cs
@@ -10,6 +10,46 @@ namespace recursion
     {
         static void Main(string[] args)
         {
+            //5! MEAN 5X4X3X2X1
+            Console.WriteLine("Factoral machine");
+            Console.Write ("please enternum");
+            int n = 0;
+            while(!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.Write("please enternum");
+            }
+
+
+            /*
+            int factorial = 1;
+
+            for(int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine($"{n} factoral is {factorial}");
+            */
+
+            int factorial = calcfact(n);
+            Console.WriteLine($"{n} factoral is {factorial}");
+
+
+
+
         }
+        static int calcfact(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+
+            }
+
+            return n * calcfact(n - 1);
+        }
+
     }
+
+
 }
